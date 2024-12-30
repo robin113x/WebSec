d428133f007cfcba50d8c2d39a98db5a78268d3a
robin113x <robin113x@gmail.com>
2024-12-30 07:23:56 -0500

    Robin h00d

--- dnschecker.sh
+++ dnschecker.sh
@@ -6,14 +6,11 @@ if [ -z "$1" ] || [ ! -f "$1" ]; then
     exit 1
 fi
 
-# Read file line by line
 while IFS= read -r line; do
-    # Check if line is not empty
     if [ -n "$line" ]; then
-        # Send request with a timeout of 5 seconds
-        curl -s --max-time 5 --head "$line" &>/dev/null
+        curl  "$line" &>/dev/null
         if [ $? -eq 0 ]; then
-            echo -e "\033[32m[+] $line is live\033[0m"   >> live_subDomain.# Green text for live
+            echo -e "\033[32m[+] $line is live\033[0m"   >> live_subDomain.txt # Green text for live
         else
             echo -e "\033[31m[-] $line is not reachable\033[0m"  # Red text for not reachable
         fi
