f34470d031cabc9e69ea937452a0a8fe9e0f4241
robin113x <robin113x@gmail.com>
2024-12-30 07:22:16 -0500

    Robin h00d

--- dnschecker.sh
+++ dnschecker.sh
@@ -6,14 +6,12 @@ if [ -z "$1" ] || [ ! -f "$1" ]; then
     exit 1
 fi
 
-# Read file line by line
 while IFS= read -r line; do
     # Check if line is not empty
     if [ -n "$line" ]; then
-        # Send request with a timeout of 5 seconds
         curl -s --max-time 5 --head "$line" &>/dev/null
         if [ $? -eq 0 ]; then
-            echo -e "\033[32m[+] $line is live\033[0m"  # Green text for live
+            echo -e "\033[32m[+] $line is live\033[0m"  
         fi
     fi
 done < "$1"
