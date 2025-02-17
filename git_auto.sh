ac074f82380e5c25e91fd67d2be5facc0d9d69c3
robin113x <robin113x@gmail.com>
2025-02-17 22:05:15 +0530

    Robin h00d

--- git_auto.sh
+++ git_auto.sh
@@ -2,6 +2,7 @@ while true; do
   
     if [[ -n $(git status --porcelain) ]]; then
         echo "Changes detected in repository. Committing and pushing changes..."
+        git pull
         git add -A
         git commit -m "Robin h00d"
         git push 
