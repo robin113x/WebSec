1cd7bed2ce129c1f5f966feb14c1427fbde82325
robin113x <robin113x@gmail.com>
2025-02-17 07:34:01 +0530

    Robin h00d

--- Recon/main.sh
+++ Recon/main.sh
@@ -1,39 +1,7 @@
 #!/bin/bash
 
-# Array of in-scope domains (replace with the full list from the previous response)
-domains=(
-    "playtika.com"
-    "slotomania.com"
-    "bingoblitz.com"
-    "playticorp.com"
-    "playwsop.com"
-    "serious.li"
-    "seriously.com"
-    "wooga.com"
-    "houseoffun.com"
-    "caesarsgames.com"
-    "junesjourney.com"
-    "solitairegrandharvest.com"
-    "bestfiends.com"
-    "buffalo-ggn.net" # Example for a non-wildcard domain
-    "boardkingsgame.com"
-    "redecor.com"
-    "justfall.lol"
-    "justplay.lol"
-    "1v1.lol"
-    "governorofpoker.com"
-    "gop3.nl"
-    "monopoly-poker.com"
-    "vegasdowntownslots.com"
-    "pokerheat.com"
-    "redocor.com"
-    "bestfiendsstars.com"
-    "jellysplash.com"
-    "switchcraft.com"
-    "tropicats.com"
-    "pearlsperil.com"
-    # ... Add all other domains
-)
+
+
 
 # Loop through the domains
 for domain in "${domains[@]}"; do
