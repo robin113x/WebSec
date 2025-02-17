35bae8d75f25b7f6838d126a9bf03dd6790f65da
robin113x <robin113x@gmail.com>
2025-02-17 07:48:03 +0530

    Robin h00d

--- Recon/main.sh
+++ Recon/main.sh
@@ -3,7 +3,7 @@
 IP_RANGES="ipRanges.txt" 
 #cat "$IP_RANGES" | dnsx -ptr -resp-only -o reverse_dns_results.txt
 
-awk '{print $NF}' reverse_dns_results.txt | anew domains.txt
+awk '{print $1}' reverse_dns_results.txt | anew domains.txt
 
 for domain in $(cat domains.txt); do
     echo "--------------------------------------------------"
