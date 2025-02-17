556e200b8168a8634275b61ff74678241189736c
robin113x <robin113x@gmail.com>
2025-02-17 07:39:39 +0530

    Robin h00d

--- Recon/main.sh
+++ Recon/main.sh
@@ -6,14 +6,16 @@ cat "$IP_RANGES" | dnsx -ptr -resp-only -o reverse_dns_results.txt
 awk '{print $NF}' reverse_dns_results.txt | anew domains.txt
 
 
-# Loop through the domains
+awk '{print $NF}' reverse_dns_results.txt | anew domains.txt
+
+# Loop through the domains and perform reconnaissance
 for domain in $(cat domains.txt); do
     echo "--------------------------------------------------"
     echo "Starting reconnaissance for: $domain"
     echo "--------------------------------------------------"
 
     # Execute your subdomain reconnaissance script
-    bash recon_script.sh "$domain"  # Replace your_recon_script.sh with the actual name of your script
+    bash recon_script.sh "$domain"  # Replace recon_script.sh with the actual name of your script
 
     echo "--------------------------------------------------"
     echo "Finished reconnaissance for: $domain"
