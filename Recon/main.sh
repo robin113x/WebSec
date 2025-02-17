a35de8dc821e0455d7330fedde7a8c7e46c022c2
robin113x <robin113x@gmail.com>
2025-02-17 07:35:20 +0530

    Robin h00d

--- Recon/main.sh
+++ Recon/main.sh
@@ -6,7 +6,7 @@ awk '{print $NF}' reverse_dns_results.txt | anew domains.txt
 
 
 # Loop through the domains
-for domain in $(); do
+for domain in $(cat domains.txt ); do
     echo "--------------------------------------------------"
     echo "Starting reconnaissance for: $domain"
     echo "--------------------------------------------------"
